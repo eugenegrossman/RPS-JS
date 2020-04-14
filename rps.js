464d3c5a50b0c4ef8d5a8b1c3510e6c5f3817e1c
eugenegrossman <gnwgrssmn@gmail.com>
2020-04-13 18:30:45 -0700

    game results display

--- rps.js
+++ rps.js
@@ -14,36 +14,59 @@ let choices = ['rock','paper','scissors']
 
   let computerwins = 0
   let yourwins = 0
+  var rounds = 0
 
+  //rounds
+  // const roundsplayed = document.querySelector('#rounds');
+  // const roundsplayed_count = document.createElement('p');
+  // roundsplayed_count.classList.add('roundsplayed');
+  // roundsplayed_count.textContent = 0;
+  // roundsplayed_count.style.color = "blue";
+
+//random computer's play
   function computerPlay(){
       let cChoice = choices[Math.floor(Math.random()*choices.length)]
       let cChoiceIndex = choices.indexOf(cChoice)
       return cChoiceIndex
   }
 
- //playing the game
-
- function playRound(computerSelection,playerSelection){
-    let result = array[computerPlay()][yourPlay()]
-    if (result === 'w'){
-      console.log("You Win!")
-      yourwins +=1;
-    } else if (result === 'l'){
-      console.log("You Lose!")
-      computerwins +=1;
-    }else{
-      console.log("It's a tie.");
-    }
-  }
-
+//your choice function, returns index of player's selection
   var yourChoice;
 
   function yourPlay(){
     let selection = yourChoice
     let selectionCase = selection.toLowerCase()
     let playerSelection = choices.indexOf(selectionCase)
-    console.log(playerSelection)
     return playerSelection
+  }
+
+ //playing the game
+ function playRound(computerSelection,playerSelection){
+    let result = array[computerPlay()][yourPlay()]
+    if (result === 'w'){
+      console.log("You Win!")
+      document.getElementById('results').innerHTML = 'Results: You Win!';
+      yourwins +=1;
+    } else if (result === 'l'){
+      console.log("You Lose!")
+      document.getElementById('results').innerHTML = 'Results: You Lose!';
+      computerwins +=1;
+    }else{
+      document.getElementById('results').innerHTML = 'Results: Tie Game.';
+      console.log("It's a tie.");
+    }
+  }
+
+//rounds played index
+function addRound() {
+  rounds += 1;
+  document.getElementById('clicks').innerHTML = rounds;
+  }
+
+//keeping score
+function scoring() {
+  document.getElementById('computerscore').innerHTML = computerwins;
+  document.getElementById('yourscore').innerHTML = yourwins;
   }
 
 //buttons
@@ -63,37 +86,10 @@ const container = document.querySelector('#container');
 const content = document.createElement('h1');
 content.classList.add('content');
 content.textContent = "RPS : Choose your weapon";
-content.style.color = "black"; 
+content.style.color = "black";
 content.style.backgroundColor = "gray";
 
 
-  function game(rounds){
-    while(rounds > 0){
-      playRound();
-      rounds -=1;
-      console.log(`There are ${rounds} rounds left.`);
-    }
-    if (computerwins > yourwins){
-      console.log(`Unlucky, you've lost ${computerwins} to ${yourwins} to the computer`);
-    }
-    else if (yourwins > computerwins){
-      console.log(`Nice, you've won ${computerwins} to ${yourwins} against the computer`);
-    }
-    else {
-      console.log("Youve tied the computer.");
-    }
-  }
-
-
-//rounds
-const roundsplayed = document.querySelector('#rounds');
-const roundsplayed_count = document.createElement('p');
-roundsplayed_count.classList.add('roundsplayed');
-roundsplayed_count.textContent = 0; 
-roundsplayed_count.style.color = "blue";
-
 //assign children
 container.appendChild(content);
-roundsplayed.appendChild(roundsplayed_count);
 
-//   game(getNumberRounds())
